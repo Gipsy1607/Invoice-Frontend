a2865b8820770ced35d1a726e2fc32de939f6e07
Gipsy1607 <swarangitarkar@gmail.com>
2023-12-03 10:39:53 +0530

    Updates

--- invoice-calculator/src/InvoiceCalculator.jsx
+++ invoice-calculator/src/InvoiceCalculator.jsx
@@ -23,39 +23,7 @@ const InvoiceCalculator = () => {
 
   return (
     <div>
-      <div>
-        <input type="text" value={input} readOnly />
-      </div>
-      <div>
-        <button onClick={() => handleButtonClick('7')}>7</button>
-        <button onClick={() => handleButtonClick('8')}>8</button>
-        <button onClick={() => handleButtonClick('9')}>9</button>
-        <button onClick={() => handleButtonClick('/')}>/</button>
-      </div>
-      <div>
-        <button onClick={() => handleButtonClick('4')}>4</button>
-        <button onClick={() => handleButtonClick('5')}>5</button>
-        <button onClick={() => handleButtonClick('6')}>6</button>
-        <button onClick={() => handleButtonClick('*')}>*</button>
-      </div>
-      <div>
-        <button onClick={() => handleButtonClick('1')}>1</button>
-        <button onClick={() => handleButtonClick('2')}>2</button>
-        <button onClick={() => handleButtonClick('3')}>3</button>
-        <button onClick={() => handleButtonClick('-')}>-</button>
-      </div>
-      <div>
-        <button onClick={() => handleButtonClick('0')}>0</button>
-        <button onClick={() => handleButtonClick('.')}>.</button>
-        <button onClick={() => handleButtonClick('=')}>=</button>
-        <button onClick={() => handleButtonClick('+')}>+</button>
-      </div>
-      <div>
-        <button onClick={() => handleButtonClick('C')}>C</button>
-      </div>
-      <div>
-        <p>Result: {result}</p>
-      </div>
+      
     </div>
   );
 };
